dde173f01cb677321fac2c39779d6b1e834b6927
toshi-toma <toshitoma1217@gmail.com>
2019-01-18 23:31:01 +0900

    DevServerの設定を追加

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,31 @@ module.exports = {
      * コンテンツの基底パス(検索先)を指定
      * 開発サーバーから配信したいコンテンツ(ex: html)が配置されているディレクトリ
     */
-    contentBase: './dist'
+    contentBase: './dist',
+
+    /**
+     * 使用するポート番号
+     * デフォルトで8080
+     */
+    port: 8080,
+
+    /**
+     * 起動時にブラウザを開くか
+     */
+    open: true,
+
+    /**
+     * 起動時に開くページ
+     */
+    openPage: '',
+
+    /**
+     * ファイル監視の設定
+     */
+    watchOptions: {
+      // 除外するフォルダ
+      ignored: /node_modules/
+    }
+
   }
 }
